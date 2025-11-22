7c7b270fc70d33eaf90d5f22aea80a4bdda18b10
son0131 <sonsh0131@gmail.com>
2025-11-22 21:46:56 +0900

    feat/#10 카테고리 선택 칸 길이 조정과 키워드 칸 꽉차게 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,13 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-route
 import Header from './components/Header.jsx';
 import Login from './pages/Login.jsx';
 import Signup from './pages/Signup.jsx';
-
-function AppContent() {
-  const location = useLocation();
-  const showHeader = location.pathname !== '/' && location.pathname !== '/signup';
 import MainPage from './pages/MainPage.jsx';
 import WritingPage from './pages/WritingPage.jsx';
 import MyPage from './pages/MyPage.jsx';
 
 function AppContent() {
   const location = useLocation();
-  const showHeader = location.pathname !== '/';
+  const showHeader = location.pathname !== '/' && location.pathname !== '/signup';
 
   return (
     <>
@@ -21,20 +17,6 @@ function AppContent() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </>
-  );
-}
-
-function App() {
-  return (
-    <Router>
-      <AppContent />
-    </Router>
-  );
-}
-  
-export default App;
         <Route path="/main" element={<MainPage />} />
         <Route path="/writing" element={<WritingPage />} />
         <Route path="/mypage" element={<MyPage />} />
@@ -50,5 +32,5 @@ function App() {
     </Router>
   );
 }
-
+  
 export default App;
